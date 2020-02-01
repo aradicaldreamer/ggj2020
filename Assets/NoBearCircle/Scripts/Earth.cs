552269a79125c0521add1b210e4b8b4ea9da8dfb
smeagolem <stooge213@hotmail.com>
2020-02-01 18:36:14 +1100

    :sparkles: add badly rotatable earth

---
+++ Assets/NoBearCircle/Scripts/Earth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Earth : OVRGrabbable
+{
+    /// <summary>
+	/// Notifies the object that it has been grabbed.
+	/// </summary>
+	override public void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        m_grabbedBy = hand;
+        m_grabbedCollider = grabPoint;
+    }
+
+    /// <summary>
+    /// Notifies the object that it has been released.
+    /// </summary>
+    override public void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        m_grabbedBy = null;
+        m_grabbedCollider = null;
+    }
+}
