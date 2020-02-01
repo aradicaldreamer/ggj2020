b0d73f29f0daf34a0faf38d10eb33dbb564628f1
MarcPagliuca <marc.pagliuca@hotmail.com>
2020-02-02 00:32:07 +1100

    Cork to volcano and towel to puddle works. Particles in too

---
+++ Assets/Marc_Art/Scripts/Volcano.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volcano : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Cork")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
