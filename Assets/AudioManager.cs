b8326233c8482741b268bf5cc2831ef25cc518c8
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2020-02-01 18:42:02 +1100

    Audio Test

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource _backgroundMusic;
+    [SerializeField] private AudioSource _voiceOver;
+    [SerializeField] private AudioClip[] _bgmLevels;
+
+    [SerializeField] private AudioClip _openingVO;
+    [SerializeField] private AudioClip[] _voiceOverTaunts;
+    [SerializeField] private AudioClip[] _voiceOverSucceeds;
+
+    private int _lastTauntPlayed = -1;
+    private bool _stopSpawning = true;
+    private bool _isTaunt = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeAudioLevel(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playVoiceOverTaunt(int tauntID)
+    {
+        _voiceOver.PlayOneShot(_voiceOverTaunts[tauntID]);
+        _lastTauntPlayed = tauntID;
+    }
+
+    public void playOpeningVoiceOver()
+    {
+        _voiceOver.PlayOneShot(_openingVO);
+    }
+
+    public void playVoiceOverSuccess()
+    {
+        if (_lastTauntPlayed == -1 )
+        {
+            Debug.LogError("Voice Over success was asked to play before a taunt");
+            return;
+        }
+        _voiceOver.PlayOneShot(_voiceOverSucceeds[_lastTauntPlayed]);
+        _lastTauntPlayed = -1;
+    }
+
+    public void StartVO()
+    {
+        _stopSpawning = false;
+        StartCoroutine(VoiceOverTest());
+    }
+    public IEnumerator VoiceOverTest()
+    {
+        yield return new WaitForSeconds(15f);
+        while (!_stopSpawning)
+        {
+            int randomTrack = Random.Range(0,6);
+            if (_isTaunt)
+            {
+                playVoiceOverTaunt(randomTrack);
+                _isTaunt = false;
+            }
+            else
+            {
+                playVoiceOverSuccess();
+                _isTaunt = true;
+            }
+            yield return new WaitForSeconds(4f);
+        }
+    }
+
+    public void changeAudioLevel(int dangerLevel)
+    {
+        _backgroundMusic.Stop();
+        
+        if (dangerLevel == 1)
+        {
+            _backgroundMusic.clip = _bgmLevels[0];
+            _backgroundMusic.loop = true;
+            _backgroundMusic.Play();
+        }
+        if (dangerLevel == 2)
+        {
+            _backgroundMusic.clip = _bgmLevels[1];
+            _backgroundMusic.loop = true;
+            _backgroundMusic.Play();
+        }
+        if (dangerLevel == 3)
+        {
+            _backgroundMusic.clip = _bgmLevels[2];
+            _backgroundMusic.loop = true;
+            _backgroundMusic.Play();
+        }
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Game Variables
+
+    // Connected GameObjects
+    private AudioManager _audioManager;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioManager = GameObject.Find("Audio_Manager").GetComponent<AudioManager>();
+        if (_audioManager == null) Debug.LogError("The Audio Manager attached to the Game Manager is NULL");
+        
+        _audioManager.playOpeningVoiceOver();
+        _audioManager.StartVO();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DebugControls();
+    }
+
+
+    void DebugControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _audioManager.changeAudioLevel(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _audioManager.changeAudioLevel(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _audioManager.changeAudioLevel(3);
+        }
+        
+    }
+}
