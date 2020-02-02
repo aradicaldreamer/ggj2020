2440d8834754619b30c9fb4d7300b7f444842ad6
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2020-02-02 12:46:34 +1100

    Added Big Red Button script

---
+++ Assets/NoBearCircle/Scripts/BigRedButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigRedButton : MonoBehaviour
+{
+    private Tool[] _tools;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tools = GameObject.FindObjectsOfType<Tool>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetTools()
+    {
+        for (int i = 0; i < _tools.Length; i++)
+        {
+            _tools[i].RespawnTool();
+        }
+    }
+}
