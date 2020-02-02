6f47e1604652d8f6c6cd42e0cfe5d021c5ec0cdb
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2020-02-02 12:28:31 +1100

    Tool script

---
+++ Assets/NoBearCircle/Scripts/Tool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour
+{
+    public Vector3 respawnLocation;
+    public Quaternion respawnRotation;
+    private Rigidbody _rigidbody;
+    
+    void Awake()
+    {
+        respawnLocation = this.transform.position;
+        respawnRotation = this.transform.rotation;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        if (_rigidbody == null)
+        {
+            Debug.LogError("Rigidbody not found on " + this.gameObject.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RespawnTool()
+    {
+        this.transform.position = respawnLocation;
+        this.transform.rotation = respawnRotation;
+        this._rigidbody.velocity = Vector3.zero;
+        this._rigidbody.angularVelocity = Vector3.zero;
+    }
+}
